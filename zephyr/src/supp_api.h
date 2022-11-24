c7c749c6e6326910998b0babba4c02c7f3caeba8
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2022-11-24 14:00:30 +0100

    wip

--- zephyr/src/supp_api.h
+++ zephyr/src/supp_api.h
@@ -34,6 +34,36 @@ int zephyr_supp_connect(const struct device *dev,
 int zephyr_supp_disconnect(const struct device *dev);
 
 /**
+ * @brief Add a network to wpa_supplicant's list of configured networks.
+ * 
+ * @param dev Wi-Fi interface to use
+ * @param params Network details
+ * @return 0 on success, negative error code on failure
+ */
+int zephyr_supp_add_network(const struct device *dev,
+			    struct wifi_connect_req_params *params);
+
+/**
+ * @brief Remove a network from wpa_supplicant's list of configured networks.
+ * @note This will disconnect if the currently connected network is to be removed.
+ * 
+ * @param dev Wi-Fi interface to use
+ * @param params Network details or NULL to remove all networks
+ * @return 0 on success, negative error code on failure
+ */
+
+int zephyr_supp_remove_network(const struct device *dev,
+			       struct wifi_connect_req_params *params);
+
+/**
+ * @brief Connect to any configured network and reconnect if necessary.
+ * 
+ * @param dev Wi-Fi interface to use
+ * @return 0 on success, negative error code on failure
+ */
+int zephyr_supp_request_connection(const struct device *dev);
+
+/**
  * @brief
  *
  * @param iface_name: Wi-Fi interface name to use
